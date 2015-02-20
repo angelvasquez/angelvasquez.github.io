a54ac2b181fc44bbf6ca381d22249653b7ed5ed7
Francotirador <francotirador.cl@gmail.com>
2015-02-19 21:41:25 -0300

    bc registro y creador de cupones

---
+++ bc-registro/js/banana.js
@@ -0,0 +1,11 @@
+$(".btn-create-modal").click(function(){
+	$(".box-ecupon").show()
+})
+
+
+$('.form-control').datepicker({
+    calendarWeeks: true,
+     format: 'mm/dd/yyyy',
+    startDate: '-3d'
+});
+
