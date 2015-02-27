742fc54ba3420533798998d08f8ea454dd81ddd3
Francotirador <francotirador.cl@gmail.com>
2015-02-27 16:27:22 -0300

    dashboard corregido

--- bc/js/banana.js
+++ bc/js/banana.js
@@ -1,23 +1,5 @@
-
-$("#trigger").click(function(e){
-	e.stopPropagation();
-	$("#sidebar-wrapper").addClass("active");
-});
-
-$("#collapse").click(function(e){
-	e.stopPropagation();
-	$("#sidebar-wrapper").removeClass("active");
-});
-
-$("#filter-btn").click(function(e){
-	e.stopPropagation();
-	$("#sidebar-wrapper").removeClass("active");
-})
-
-
-jQuery(document).ready(function( $ ) {
-	$('span').counterUp({
-		delay: 10, // the delay time in ms
-		time: 1000 // the speed time in ms
-	});
+//TREE TABLE
+$(document).ready(function() {
+    $('.tree').treegrid({initialState: 'collapsed'});
+    $('.selectpicker').selectpicker();
 });
