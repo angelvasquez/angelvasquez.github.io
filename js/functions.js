099eec36fbf78cf60038550090f7e66d85eb7d79
Angel Vásquez <francotirador.cl@gmail.com>
2016-12-08 23:08:37 -0300

    almuerzo

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-	var elements = new Array;
 	var tagList = new Array;
 	var listSelected;
 	var elemSelected;
@@ -10,46 +9,47 @@ $(document).ready(function(){
 	var left;
 
 
-	var elements = $('.panel > .data-panel > .resources > .trigger-cont > .trigger');
+	var elements = $('.trigger');
 
-		$(elements).each(function(elemIndex){
-			$(this).on("click", function(){
-				selected = elemIndex;
-				var position = $(this).position();
-				var top = position.top;
-				var left = position.left;
-				top = top + 30;
-				$(".modal").css({
-					top: top+"px",
-					left: left+"px",
-					display: 'block'
-				});
-			});//end trigger modal
-
-			$(".tags").on('click', "i", function(){
-				$(this).parent().remove();
-			});
-		});
-
-
-		$('#addElement').on('click', function(){
-			listSelected = $("resources-"+selected);
-			object = $("#newElement").val();
-			tagList = object.split(',');
-			if($('#newElement').val() == ""){
-				alert("Please add some resources");
-			}else{
-				for(var i = 0; i < tagList.length; i++){
-					$('#tags-'+selected).append("<li id="+tagList[i]+">"+tagList[i]+" <i class='fa fa-times'></i></li>");
-				 }
-			}
-		});
-
-
-		$("#close-modal").on('click', function(){
+	$('.trigger').each(function(elemIndex){
+		$(this).on("click", function(){
+			console.log(elemIndex);
+			selected = elemIndex;
+			var position = $(this).position();
+			var top = position.top;
+			var left = position.left;
+			top = top + 30;
 			$(".modal").css({
-				display: 'none'
+				top: top+"px",
+				left: left+"px",
+				display: 'block'
 			});
-			$('#newElement').val("");
+		});//end trigger modal
+
+		$(".tags").on('click', "i", function(){
+			$(this).parent().remove();
 		});
+	});
+
+
+	$('#addElement').on('click', function(){
+		listSelected = $("resources-"+selected);
+		object = $("#newElement").val();
+		tagList = object.split(',');
+		if($('#newElement').val() == ""){
+			alert("Please add some resources");
+		}else{
+			for(var i = 0; i < tagList.length; i++){
+				$('#tags-'+selected).append("<li id="+tagList[i]+">"+tagList[i]+" <i class='fa fa-times'></i></li>");
+			 }
+		}
+	});
+
+
+	$("#close-modal").on('click', function(){
+		$(".modal").css({
+			display: 'none'
+		});
+		$('#newElement').val("");
+	});
 });
